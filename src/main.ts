887e023a9e1c3880e214a494b88796566244b1ae
Bryan Cruz <bryanerickcruznina@gmail.com>
2023-05-18 18:03:06 -0400

    michitos listos

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule/*,{cors:true}*/);
   
   const config = new DocumentBuilder()
     .setTitle('API Personal')
@@ -17,7 +18,20 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app,config)
     SwaggerModule.setup('/api',app,document)
-  
+    
+    const allowedOrigins = ['http://localhost:3000', 'https://known-kitten-production.up.railway.app'];
+
+    app.use(cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      optionsSuccessStatus: 200
+    }));
+
   await app.listen(3000);
 }
 bootstrap();
